7e0b97d0bf58822463ed33d63952c3aa31f947a6
VivianChan-TH <63438624+VivianChan-TH@users.noreply.github.com>
2023-10-06 11:47:10 +0800

    init primary db tables

--- backend/app.py
+++ backend/app.py
@@ -17,10 +17,43 @@ class Question(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     question=db.Column(db.String(255), nullable=False)
     answer=db.Column(db.Boolean, nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
     
     def __init__(self, question, answer):
         self.question=question
         self.answer=answer
+class Course(db.Model):
+    __tablename__='courses'
+    id=db.Column(db.Integer, primary_key=True)
+    course_name=db.Column(db.String(255), nullable=False)
+    questions=db.relationship('Question', backref='course', lazy=True)
+    
+    def __init__(self, course_name, questions):
+        self.course_name=course_name
+        self.questions=questions
+class RegisteredCourse(db.Model):
+    __tablename__='registered_courses'
+    id=db.Column(db.Integer, primary_key=True)
+    course_id=db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
+    student_id=db.Column(db.Integer,db.ForeignKey('students.id'),nullable=False)
+    score=db.Column(db.Integer,nullable=False)
+
+    def __init__(self, score):
+        self.score=score
+class Student(db.Model):
+    __tablename__='students'
+    id=db.Column(db.Integer, primary_key=True)
+    student_name=db.Column(db.String(255), nullable=False)
+    password=db.Column(db.String(255), nullable=False)
+    age_group=db.Column(db.String(255), nullable=False)
+    courses=db.relationship('registered_courses', backref='student', lazy=True)
+    
+    def __init__(self, student_name,password, age_group):
+        self.student_name=student_name
+        self.password=password
+        self.age_group=age_group
+
+
 
 # sample hello world page
 @app.route('/')
