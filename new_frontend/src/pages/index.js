18eb434b4e992045caead710acd9d1c707c16a19
Yonghyun Kwon <yonghyunkwon98@gmail.com>
2023-10-06 15:54:22 +0800

    first commit

--- new_frontend/src/pages/index.js
+++ new_frontend/src/pages/index.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Link } from "react-router-dom";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous"
+      ></link>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+        <div>
+          <Link to={`/login_page`} className="btn btn-primary mr-3">
+            For Students
+          </Link>
+          <Link to={`/login_page`} className="btn btn-primary">
+            For Teachers
           </Link>
         </div>
       </div>
@@ -27,14 +37,37 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Description will go into a meta tag in <head />"
+    >
       <HomepageHeader />
       <main>
-        <HomepageFeatures />
+        <Row className="p-4">
+          <Col>
+            <img
+              src="https://i.pinimg.com/originals/48/93/c2/4893c2e62865ad38307dc554018101a5.jpg"
+              style={{ width: "500px", height: "400px" }}
+            />
+            <h4>Early financial education empowers kids.</h4>
+          </Col>
+          <Col>
+            <img
+              src="https://i.pinimg.com/564x/8b/da/d5/8bdad513adb8b008cdd4fd00b1a24ad6.jpg"
+              style={{ width: "500px", height: "400px" }}
+            />
+            <h4>Money skills secure their future.</h4>
+          </Col>
+          <Col>
+            <img
+              src="https://i.pinimg.com/564x/b1/93/51/b19351463a92831db196f5e09c2cd448.jpg"
+              style={{ width: "500px", height: "400px" }}
+            />
+            <h4>Home talks build financial foundations.</h4>
+          </Col>
+        </Row>
       </main>
     </Layout>
   );
