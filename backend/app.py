4a62ea0375382bfa9b78f840e4ecc07d03ddb764
aidan215 <99425675+aidan215@users.noreply.github.com>
2023-10-06 11:13:02 +0800

    setup for database for questions

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from 
+from flask_sqlalchemy import SQLAlchemy 
 
 # declare constants
 HOST = '0.0.0.0'
@@ -7,6 +7,20 @@ PORT = 5000
 
 # initialize flask application
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:CFG2023@localhost/questions'
+
+db=SQLAlchemy(app)
+
+class Question(db.Model):
+    __tablename__='questions'
+    id=db.Column(db.Integer, primary_key=True)
+    question=db.Column(db.String(255), nullable=False)
+    answer=db.Column(db.Boolean, nullable=False)
+    
+    def __init__(self, question, answer):
+        self.question=question
+        self.answer=answer
 
 # sample hello world page
 @app.route('/')
